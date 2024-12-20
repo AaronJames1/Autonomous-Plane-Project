8799bf826431349b6443e629873a9aec3f592ae1
Aaron James <aaronjames4123@gmail.com>
2024-12-20 17:57:56 -0500

    Added Coordinate Transformer to convert coordinates from WGS84 to Web Mercator coordinate systems.

--- Plane_Plotter/Plane_Project_Plotter_Code.py
+++ Plane_Plotter/Plane_Project_Plotter_Code.py
@@ -1,14 +1,21 @@
 from pyautocad import Autocad, APoint
+from pyproj import Proj, Transformer
 
 # Initialize AutoCAD
 acad = Autocad()
 
-# Get user input (you can customize this part)
-x = float(input("Enter X coordinate: "))
-y = float(input("Enter Y coordinate: "))
+while True:
 
-# Create an AutoCAD point
-point = APoint(x, y)
+    # Get user input 
+    x = float(input("Enter Latitude coordinate: "))
+    y = float(input("Enter Longitude coordinate: "))
 
-# Plot the point in AutoCAD
-acad.model.AddPoint(point)
+    # Convert from WGS84 to Web Mercator (meters)
+    transformer = Transformer.from_crs('EPSG:4326', 'EPSG:3857')
+    lat, lon = transformer.transform(x, y)
+
+    # Create an AutoCAD point
+    point = APoint(lat, lon)
+
+    # Plot the point in AutoCAD
+    acad.model.AddPoint(point)
