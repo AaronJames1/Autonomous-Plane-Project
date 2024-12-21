7ce07f82c1b20dd9f49b7fa479a107b22053d1af
Aaron James <aaronjames4123@gmail.com>
2024-12-20 23:04:28 -0500

    Added .csv file reader and changed the script to also draw lines between AutoCAD points

--- Plane_Plotter/Plane_Project_Plotter_Code.py
+++ Plane_Plotter/Plane_Project_Plotter_Code.py
@@ -1,21 +1,33 @@
 from pyautocad import Autocad, APoint
 from pyproj import Proj, Transformer
+import csv
 
 # Initialize AutoCAD
 acad = Autocad()
 
-while True:
+points = []
 
-    # Get user input 
-    x = float(input("Enter Latitude coordinate: "))
-    y = float(input("Enter Longitude coordinate: "))
+#Opening the CSV File
+with open('test.csv') as file:
+    data = csv.reader(file)
+    for lines in data:
+        x = lines[0]
+        y = lines[1]
 
-    # Convert from WGS84 to Web Mercator (meters)
-    transformer = Transformer.from_crs('EPSG:4326', 'EPSG:3857')
-    lat, lon = transformer.transform(x, y)
+        # Convert from WGS84 to Web Mercator (meters)
+        transformer = Transformer.from_crs('EPSG:4326', 'EPSG:3857')
+        lat, lon = transformer.transform(x, y)
 
-    # Create an AutoCAD point
-    point = APoint(lat, lon)
+        # Creates and stores point
+        point = APoint(lat, lon)
+        points.append(point)
+
+for i in range(len(points)-1):
 
     # Plot the point in AutoCAD
     acad.model.AddPoint(point)
+
+    # Plot the lines in AutoCAD
+    acad.model.AddLine(points[i],points[i+1])
+
+print("complete")
